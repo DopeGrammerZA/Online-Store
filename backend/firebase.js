5de8ab9bfaf1975f313e63dbe4f8733f2ba6c198
DopeGrammer <Kagisomasebe98@gmail.com>
2024-11-04 12:27:42 +0200

    Added hhtp post request functionality, link the server with firebase and also added a route to test

---
+++ backend/firebase.js
@@ -0,0 +1,15 @@
+const admin = require('firebase-admin');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: ' firebase-adminsdk-pc32k@online-store-f8d98.iam.gserviceaccount.com',
+});
+
+const db = admin.firestore();
+
+module.exports = db;
