6fe7b97e1999a58ebc65978fd9dfdca9bd55638b
DopeGrammer <Kagisomasebe98@gmail.com>
2024-11-04 12:50:18 +0200

    Added authController, defined its routes and also added other product-endpoint

---
+++ backend/controllers/authController.js
@@ -0,0 +1,31 @@
+const admin = require('firebase-admin');
+const db = require('../firebase.js');
+
+
+exports.register = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const userRecord = await admin.auth().createUser({
+            email,
+            password,
+            displayName: username,
+        });
+
+        res.status(201).json({ message: 'User registered successfully', user: userRecord });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email } = req.body; 
+    try {
+        const userRecord = await admin.auth().getUserByEmail(email);
+        res.json({
+            message: 'Login successful',
+            user: { uid: userRecord.uid, username: userRecord.displayName, email: userRecord.email },
+        });
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid credentials' });
+    }
+};
