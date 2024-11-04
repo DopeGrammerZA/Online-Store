9a86fb65f6db9cc519d419986b0650c495e5ddf6
DopeGrammer <Kagisomasebe98@gmail.com>
2024-11-04 13:34:32 +0200

    Initialized React-App and also installed React-Bootstrap for styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from 'react-bootstrap/Button';
+
+function App() {
+    return (
+        <div className="App">
+            <h3>Something Amazing will be Constructed here....</h3>
+            <h1>Welcome to our Online Store</h1>
+            <Button variant="primary">Click Me!</Button>
+        </div>
+    );
+}
+
+export default App;
